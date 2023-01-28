c26345bb99392ac6fe7a2e1518b2f3f153ea9c38
Valerio Scisci <valerio.scisci@email.it>
2023-01-28 17:40:53 +0100

    Fixed prod backend env url

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,5 @@ export const env = {
   API_URL:
     String(process.env.NODE_ENV) == "development"
       ? "http://localhost:3000/api"
-      : "https://vercel.com/valerioscisci/3bee-test-der6/api",
+      : "https://3bee-test-der6.vercel.app/api",
 };
